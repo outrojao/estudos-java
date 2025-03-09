c72588353ecd9370ff78345fccb050ebdf954118
João Victor <joaovictordsantosr@gmail.com>
2025-03-09 17:35:10 -0300

    vetores

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,46 +2,32 @@ package entities;
 
 public class Product {
 	public static int generalId = 0;
-	public int id = ++generalId;
-	public String name;
-	public double price;
-	public int quantity;
-	
-	
-	public double TotalValueInStock() {
-		return (double) price * quantity;
-	}
-	
+	private int id = ++generalId;
+	private String name;
+	private double price;
+
 	public int getId() {
 		return this.id;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getPrice() {
 		return this.price;
 	}
-	
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	public int getQuantity() {
-		return this.quantity;
-	}
-	
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	
+
 	public String toString() {
-		return "ID: " + this.id + ", NAME: " + this.name + ", PRICE: " + this.price + ", QUANTITY: " + this.quantity;	
+		return "ID: " + this.id + ", NAME: " + this.name + ", PRICE: " + this.price;
 	}
 
 }
--- src/estudos/Main.java
+++ src/estudos/Main.java
@@ -3,39 +3,127 @@ package estudos;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.BankAccount;
+//import entities.Product;
+import entities.Person;
 
 public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		BankAccount bankAccount;
-		System.out.print("Account number: ");
-		int accountNumber = sc.nextInt();
+//		System.out.print("QUANTIDADE DE PRODUTOS: ");
+//		int n = sc.nextInt();
+//		Product[] vect = new Product[n];
+//		System.out.println();
+//
+//		for (int i = 0; i < n; i++) {
+//			System.out.print("NOME DO PRODUTO: ");
+//			String name = sc.nextLine();
+//			sc.next();
+//			System.out.print("PREÇO DO PRODUTO: ");
+//			double price = sc.nextDouble();
+//			Product product = new Product();
+//			product.setName(name);
+//			product.setPrice(price);
+//			vect[i] = product;
+//			System.out.println();
+//		}
+//
+//		double sum = 0.0;
+//		for (int i = 0; i < n; i++) {
+//			sum += vect[i].getPrice();
+//		}
+//		double avg = sum / n;
+//
+//		System.out.println("MÉDIA DE PREÇOS: " + avg);
+
+//		System.out.print("QUANTIDADE DE NÚMEROS INTEIROS: ");
+//		int n = sc.nextInt();
+//		int[] vect = new int[n];
+//		System.out.println();
+//		
+//		for (int i = 0; i < n; i++) {
+//			vect[i] = sc.nextInt();
+//		}
+//		
+//		System.out.println("NÚMEROS NEGATIVOS:");
+//		for (int i = 0; i < n; i++) {
+//			if(vect[i] < 0) {
+//				System.out.println(vect[i]);
+//			}
+//		}	
+
+//		System.out.print("QUANTIDADE DE PESSOAS: ");
+//		int n = sc.nextInt();
+//
+//		Person[] vect = new Person[n];
+//
+//		for (int i = 0; i < n; i++) {
+//			System.out.println("DADOS DA " + (i + 1) + "ª PESSOA:");
+//			Person person = new Person();
+//
+//			System.out.print("NOME: ");
+//			sc.nextLine();
+//			String name = sc.nextLine();
+//
+//			System.out.print("IDADE: ");
+//			int age = sc.nextInt();
+//
+//			System.out.print("ALTURA: ");
+//			double height = sc.nextDouble();
+//
+//			person.setName(name);
+//			person.setAge(age);
+//			person.setHeight(height);
+//
+//			vect[i] = person;
+//			System.out.println();
+//		}
+//
+//		int qtd16 = 0;
+//		System.out.println("PESSOAS COM MENOS DE 16:");
+//		for (int i = 0; i < n; i++) {
+//			if (vect[i].getAge() < 16) {
+//				qtd16++;
+//				System.out.println(vect[i].getName());
+//			}
+//		}
+//
+//		double percent16 = 100 * qtd16 / n;
+//		System.out.println("EQUIVALE A " + percent16 + "%!");
+
+		System.out.print("QUANTIDADE DE PESSOAS: ");
+		int n = sc.nextInt();
 		sc.nextLine();
-		System.out.print("Account holder: ");
-		String accountHolder = sc.nextLine();
-		System.out.print("Make a initial deposit [S/N]? ");
-		if (sc.next().toUpperCase().equals("S")) {
-			System.out.print("Initial deposit value: ");
-			double initialDeposit = sc.nextDouble();
-			bankAccount = new BankAccount(accountNumber, accountHolder, initialDeposit);
 
-		} else {
-			bankAccount = new BankAccount(accountNumber, accountHolder);
+		Person[] vect = new Person[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("DADOS DA " + (i + 1) + "ª PESSOA:");
+			Person person = new Person();
+
+			System.out.print("NOME: ");
+			String name = sc.nextLine();
+
+			System.out.print("IDADE: ");
+			int age = sc.nextInt();
+			sc.nextLine();
+
+			person.setName(name);
+			person.setAge(age);
+
+			vect[i] = person;
+			System.out.println();
 		}
-		System.out.println("\nAccount created successfully!");
-		System.out.printf("%nACCOUNT NUMBER: %d - ACCOUNT HOLDER: %s - ACCOUNT BALANCE: %.2f",
-				bankAccount.getAccountNumber(), bankAccount.getAccountHolder(), bankAccount.getBalance());
 
-		bankAccount.depositFunds(100);
-		System.out.printf("%nACCOUNT NUMBER: %d - ACCOUNT HOLDER: %s - ACCOUNT BALANCE: %.2f",
-				bankAccount.getAccountNumber(), bankAccount.getAccountHolder(), bankAccount.getBalance());
+		int idade = 0;
+		for (int i = 0; i < n; i++) {
+			if (vect[i].getAge() > idade) {
+				idade = vect[i].getAge();
+			}
+		}
+		System.out.println(idade);
 
-		bankAccount.withdrawFunds(200);
-		System.out.printf("%nACCOUNT NUMBER: %d - ACCOUNT HOLDER: %s - ACCOUNT BALANCE: %.2f",
-				bankAccount.getAccountNumber(), bankAccount.getAccountHolder(), bankAccount.getBalance());
 		sc.close();
 	}
 }
